06764fb193e2be671cbcdc9ade5a22135ac53007
viktoriagecse <viktoriagecse3@gmail.com>
2017-09-24 11:49:09 +0200

    first commit & push

---
+++ src/main/java/hu/elte/alkfejl/issuetracker/IssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.alkfejl.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//comment
+public class IssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerApplication.class, args);
+	}
+}
